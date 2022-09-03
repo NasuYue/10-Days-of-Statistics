13bed0976ca25ecc1f9e0c1aa0d1ab2ee013eb6f
那須ゆえ <NasuYue@users.noreply.github.com>
2022-09-03 23:59:02 +0800

    d7

--- day6_The-Central-Limit-Theorem-I.py
+++ day6_The-Central-Limit-Theorem-I.py
@@ -1,10 +1,9 @@
 # Task
-# A large elevator can transport a maximum of 9800 pounds. Suppose a load of cargo containing 49 boxes must be transported via the elevator. The box weight of this type of cargo follows a distribution with a mean of mu=205  pounds and a standard deviation of sigma=15 pounds. Based on this information, what is the probability that all 49 boxes can be safely loaded into the freight elevator and transported?
+# A large elevator can transport a maximum of 9800 pounds. Suppose a load of cargo containing 49 boxes must be transported via the elevator. The box weight of this type of cargo follows a distribution with a mean of mu=205 pounds and a standard deviation of sigma=15 pounds. Based on this information, what is the probability that all 49 boxes can be safely loaded into the freight elevator and transported?
 
-# Central limit theorem(CLT) refer to sample mean follows normal distribution as long as n is large enough.
+# Central limit theorem(CLT) refer to sample mean follows normal distribution with a large N.
 # sample sum follows Norm(mu',std') 
-# Sample mean mu' = n*mu; 
-# sample std std' = sqrt(n)*sigma
+# mu' = n*mu; std' = sqrt(n)*sigma, 
 
 
 import statistics as st
---
+++ day7_Pearson-Correlation-Coefficient-I.py
@@ -0,0 +1,27 @@
+# Task
+# Given two n-element data sets, X and Y, calculate the value of the Pearson correlation coefficient.
+import statistics as stats
+
+# Test sampe
+# n=10
+# X=[10, 9.8, 8, 7.8, 7.7, 7, 6, 5, 4, 2]
+# Y=[200, 44, 32, 24, 22, 17, 15, 12, 8, 4]
+
+n=int(input())
+X=list(map(float,input().split()))
+Y=list(map(float,input().split()))
+
+mean_x=round(sum(X)/len(X),3)
+mean_y=round(sum(Y)/len(Y),3)
+
+# Be aware: use population stdev pstdev()
+std_x=round(stats.pstdev(X),3)
+std_y=round(stats.pstdev(Y),3)
+
+sum_of_covar=0
+for i in range(n):
+    sum_of_covar+=(X[i]-mean_x)*(Y[i]-mean_y)
+
+corr=round(sum_of_covar/(std_x*std_y*n),3)
+
+print(corr)
